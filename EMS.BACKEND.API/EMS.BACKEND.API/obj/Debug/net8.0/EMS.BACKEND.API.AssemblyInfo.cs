e76dab7e36e0f36f5bb26ae56b0d9527bcddbb8d
Bhanuka Anjana <bhanuka2500@gmail.com>
2024-01-16 04:06:17 +0530

    add update method to interface

--- EMS.BACKEND.API/EMS.BACKEND.API/obj/Debug/net8.0/EMS.BACKEND.API.AssemblyInfo.cs
+++ EMS.BACKEND.API/EMS.BACKEND.API/obj/Debug/net8.0/EMS.BACKEND.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS.BACKEND.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e502eae20ac6ae56017fa29abe24639ac70726f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fd0ae3f1151a72d8070f16da9ef15f9390e1655")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS.BACKEND.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS.BACKEND.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
